3251e178bb19ca34b67437ef69395d71e8a06b6a
magzdev <magdakloskowicz@gmail.com>
2023-01-03 14:07:40 +0000

    add code to generate a computer choice

--- game.js
+++ game.js
@@ -1,15 +1,32 @@
+//new variables
 const computerChoice = document.getElementById('computer-choice');
-const useryChoice= document.getElementById('user-choice');
+const userChoice= document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
-const possibleChoices = document.getElementsByTagName('button');
+const possibleChoices = document.querySelectorAll('button');
 let user;
 let computer;
 let result;
 
-//
-possibleChoices.forEach(buttons => buttons.addEventListener('click', (event) => {
-  userChoice = event.target.id
+// add a click event for all the buttons
+possibleChoices.forEach(button => button.addEventListener('click', (event) => {
+  user = event.target.id // get id of a button clicked
   userChoice.innerHTML = user
   getComputerChoice()
   getResult()
 }))
+
+// generate a computer choice
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) // get a random integer
+  if (randomNumber === 0) {
+    computer = 'rock';
+  }
+ else if (randomNumber === 1) {
+    computer = 'scissors';
+  }
+ else if (randomNumber === 2) {
+    computer = 'paper';
+  }
+  computerChoice.innerHTML = computer
+
+}
